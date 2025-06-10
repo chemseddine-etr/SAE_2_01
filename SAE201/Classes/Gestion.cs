2584a35fea8239096f19e958b3447b56792ce807
Leo Morard <Leo.Morard@etu.univ-smb.fr>
2025-06-10 17:15:03 +0200

    fk pour categorie

--- SAE201/Classes/Gestion.cs
+++ SAE201/Classes/Gestion.cs
@@ -1,100 +1,79 @@
-﻿using System;
+﻿using SAE201.Classes;
+using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SAE201.Classes
+public class Gestion
 {
-    public class Gestion
+    private string nom;
+    private ObservableCollection<Client> lesClients;
+    private ObservableCollection<Plat> lesPlats;
+    private ObservableCollection<Employe> lesEmploye;
+    private ObservableCollection<Commande> lesCommande;
+    private ObservableCollection<Periode> lesPeriodes;
+    private ObservableCollection<SousCategorie> lesSousCategories;
+    private ObservableCollection<Role> lesRoles;
+    private ObservableCollection<Categorie> lesCategories;
+
+    public Gestion() : this("")
     {
-        private string nom;
-        private ObservableCollection<Client> lesClients;
-        private ObservableCollection<Plat> lesPlats;
-        private ObservableCollection<Employe> lesEmploye;
-        private ObservableCollection<Commande> lesCommande;
-        private ObservableCollection<Periode> lesPeriodes;
-        private ObservableCollection<SousCategorie> lesSousCategorie;
-        private ObservableCollection<Role> lesRoles;
-        private ObservableCollection<Categorie> lesCategorie;
+    }
 
-        public Gestion():this("")
-        {
-        }
+    public Gestion(string nom)
+    {
+        this.Nom = nom;
+        this.LesClients = new ObservableCollection<Client>(new Client().FindAll());
+        this.LesPlats = new ObservableCollection<Plat>(new Plat().FindAll());
+        this.LesEmploye = new ObservableCollection<Employe>(new Employe().FindAll());
+        this.LesCommande = new ObservableCollection<Commande>(new Commande().FindAll());
+        this.LesCategories = new ObservableCollection<Categorie>(new Categorie().FindAll());
+        this.LesSousCategories = new ObservableCollection<SousCategorie>(new SousCategorie().FindAll(this));
+    }
 
-        public Gestion(string nom)
-        {
-            this.Nom = nom;
-            this.LesClients = new ObservableCollection<Client>(new Client().FindAll());
-            this.LesPlats = new ObservableCollection<Plat>(new Plat().FindAll());
-            this.LesEmploye = new ObservableCollection<Employe>(new Employe().FindAll());
-            this.LesCommande = new ObservableCollection<Commande>(new Commande().FindAll());
-        }
+    public ObservableCollection<Client> LesClients
+    {
+        get { return this.lesClients; }
+        set { this.lesClients = value; }
+    }
 
-        public ObservableCollection<Client> LesClients
-        {
-            get
-            {
-                return this.lesClients;
-            }
+    public ObservableCollection<Plat> LesPlats
+    {
+        get { return this.lesPlats; }
+        set { this.lesPlats = value; }
+    }
 
-            set
-            {
-                this.lesClients = value;
-            }
-        }
+    public ObservableCollection<Employe> LesEmploye
+    {
+        get { return this.lesEmploye; }
+        set { this.lesEmploye = value; }
+    }
 
-        public ObservableCollection<Plat> LesPlats
-        {
-            get
-            {
-                return this.lesPlats;
-            }
+    public ObservableCollection<Commande> LesCommande
+    {
+        get { return this.lesCommande; }
+        set { this.lesCommande = value; }
+    }
 
-            set
-            {
-                this.lesPlats = value;
-            }
-        }
+    public ObservableCollection<Categorie> LesCategories
+    {
+        get { return this.lesCategories; }
+        set { this.lesCategories = value; }
+    }
 
-        public ObservableCollection<Employe> LesEmploye
-        {
-            get
-            {
-                return this.lesEmploye;
-            }
+    public ObservableCollection<SousCategorie> LesSousCategories
+    {
+        get { return this.lesSousCategories; }
+        set { this.lesSousCategories = value; }
+    }
 
-            set
-            {
-                this.lesEmploye = value;
-            }
-        }
-
-        public ObservableCollection<Commande> LesCommande
-        {
-            get
-            {
-                return this.lesCommande;
-            }
-
-            set
-            {
-                this.lesCommande = value;
-            }
-        }
-
-        public string Nom
-        {
-            get
-            {
-                return this.nom;
-            }
-
-            set
-            {
-                this.nom = value;
-            }
-        }
+    public string Nom
+    {
+        get { return this.nom; }
+        set { this.nom = value; }
     }
 }
+
+
--- SAE201/userControls/Rechercherplat.xaml.cs
+++ SAE201/userControls/Rechercherplat.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SAE201.Classes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,11 +24,57 @@ namespace SAE201
         public Rechercherplat()
         {
             InitializeComponent();
+            dgPlats.Items.Filter = FiltrePlatCombine;
         }
 
-        private void rechercher_Click(object sender, RoutedEventArgs e)
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
         {
-
+            CollectionViewSource.GetDefaultView(dgPlats.ItemsSource).Refresh();
         }
+
+        private void textMotClefPlat_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            CollectionViewSource.GetDefaultView(dgPlats.ItemsSource).Refresh();
+        }
+
+        private bool FiltrePlatCombine(object obj)
+        {
+            var unPlat = obj as Plat; // Assurez-vous d'avoir une classe Plat appropriée
+            if (unPlat == null) return false;
+
+            // Filtrage par mot-clé
+            bool motClefMatch = string.IsNullOrEmpty(textMotClefPlat.Text) ||
+                                unPlat.Nomplat.ToLower().Contains(textMotClefPlat.Text.ToLower());
+
+            // Filtrage par catégorie
+            bool categorieMatch = true;
+            if (radioEntree.IsChecked == true && unPlat.Categorie != "Entrée")
+                categorieMatch = false;
+            if (radioPlat.IsChecked == true && unPlat.Categorie != "Plat")
+                categorieMatch = false;
+
+            // Filtrage par sous-catégorie
+            bool sousCategorieMatch = true;
+            if (radioEntreeChaude.IsChecked == true && unPlat.SousCategorie != "Entrée chaude")
+                sousCategorieMatch = false;
+            if (radioEntreeFroide.IsChecked == true && unPlat.SousCategorie != "Entrée froide")
+                sousCategorieMatch = false;
+            if (radioPlatChaud.IsChecked == true && unPlat.SousCategorie != "Plat chaud")
+                sousCategorieMatch = false;
+            if (radioPlatFroid.IsChecked == true && unPlat.SousCategorie != "Plat froid")
+                sousCategorieMatch = false;
+
+            // Filtrage par disponibilité
+            bool disponibiliteMatch = string.IsNullOrEmpty(textDisponibilite.Text) ||
+                                      unPlat.Disponibilite.ToString().Contains(textDisponibilite.Text);
+
+            // Filtrage par prix
+            bool prixMatch = string.IsNullOrEmpty(textPrix.Text) ||
+                             (decimal.TryParse(textPrix.Text, out decimal prixMax) && unPlat.Prixunitaire <= prixMax);
+
+            // Retourner vrai seulement si toutes les conditions sont satisfaites
+            return motClefMatch && categorieMatch && sousCategorieMatch && disponibiliteMatch && prixMatch;
+        }
+
     }
 }
